744324a86fd51d84b4db3aa4ad67bd30a31e8eac
Gajju45 <gajendrasharma421@gmail.com>
2021-06-25 12:47:32 +0530

    BMI Next Activity Color

--- app/src/main/java/com/android/gajju45/bmicalculator/ResultActivity.java
+++ app/src/main/java/com/android/gajju45/bmicalculator/ResultActivity.java
@@ -20,10 +20,12 @@ public class ResultActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         Float bmiObtain = extras.getFloat("RESULT");
-        String bmiStatusObtain = extras.getString("TEXTBMI");
+
+
+        Float bmiStatusObtain = extras.getFloat("TEXTBMI");
         String bmiObtainString = Float.toString(bmiObtain);
+        bmiStatusTV.setText(interpreteBMI(bmiObtain));
         bmiDataTV.setText(bmiObtainString);
-        bmiStatusTV.setText(bmiStatusObtain);
 
 
     }
@@ -32,5 +34,22 @@ public class ResultActivity extends AppCompatActivity {
     private void initViews() {
         bmiDataTV = (TextView) findViewById(R.id.reult_data_tv);
         bmiStatusTV = (TextView) findViewById(R.id.result_bmi_status_tv);
+
+
+    }
+
+    public String interpreteBMI(float bmiValue) {
+        if (bmiValue < 18.5) {
+            bmiStatusTV.setBackgroundResource(R.color.under_weight);
+            return "Under weight";
+        } else if (bmiValue < 25) {
+            bmiStatusTV.setBackgroundResource(R.color.normal_weight);
+            return "Normal weight";
+        } else if (bmiValue < 30) {
+            bmiStatusTV.setBackgroundResource(R.color.over_weight);
+            return "Over Weight weight";
+        } else
+            bmiStatusTV.setBackgroundResource(R.color.obese);
+        return "Obese";
     }
 }
